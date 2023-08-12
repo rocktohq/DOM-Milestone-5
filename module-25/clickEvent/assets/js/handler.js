fdb42b20ebbee96441dcce40f2227b674ecb1a99
Saidul Mursalin <itzmonir@gmail.com>
2023-08-12 22:54:41 +0600

    Handler Added

---
+++ module-25/clickEvent/assets/js/handler.js
@@ -0,0 +1,9 @@
+function handlerOnClick() {
+    const handlerStatus = document.getElementById("handler-status");
+    handlerStatus.innerText = "Text changed by hander";
+}
+
+const changeMsg = document.getElementById("changeMsg").addEventListener("click", function () {
+    const handerMsg = document.getElementById("handler-msg");
+    handerMsg.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, temporibus laboriosam culpa quas sint iusto exercitationem perferendis cum eaque iste.";
+});
