1e93de55258ccc7aea16f16b5165ffac3fa4e6ea
Saidul Mursalin <itzmonir@gmail.com>
2023-08-16 03:51:42 +0600

    Task-4: Fruit List

---
+++ module-25/task-3/list.js
@@ -0,0 +1,13 @@
+const ul = document.querySelector("#fruit-list");
+
+const fruit1 = document.createElement("li");
+fruit1.innerText = "Apple 🍎";
+ul.appendChild(fruit1);
+
+const fruit2 = document.createElement("li");
+fruit2.innerText = "Banana 🍌";
+ul.appendChild(fruit2);
+
+const fruit3 = document.createElement("li");
+fruit3.innerText = "Grapes 🍇";
+ul.appendChild(fruit3);
