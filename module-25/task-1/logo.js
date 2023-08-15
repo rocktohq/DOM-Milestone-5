20de04aafb3540882dac9143dc156ba9a8d767c5
Saidul Mursalin <itzmonir@gmail.com>
2023-08-16 03:14:05 +0600

    Task -1

---
+++ module-25/task-1/logo.js
@@ -0,0 +1,8 @@
+const div = document.querySelector("div");
+
+const image = document.createElement("img");
+image.src = "img/oppenheimer1.png";
+image.classList.add("border-r");
+image.style.width = "100%";
+
+div.appendChild(image);
